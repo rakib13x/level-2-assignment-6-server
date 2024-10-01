abbeea96d342a8cee700b441f57e54dd688aacd1
rakib13x <rakib13x@gmail.com>
2024-10-02 04:38:57 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import path from 'path';
+// import globalErrorHandler from './app/middlewares/globalErrorHandler';
+// import notFound from './app/middlewares/middleware';
+// import router from './app/routes';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+
+// application routes
+// app.use('/api/v1', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from server.');
+});
+
+// app.use(globalErrorHandler);
+// app.use(notFound);
+export default app;
